66004a79ef7fe5cef0e635b6fee6fa2fca933e8a
Handbandlisot <aufilin@list.ru>
2026-02-18 11:20:02 +0700

    Первая половина практикума

---
+++ Practicum8/11.py
@@ -0,0 +1,15 @@
+text = 'Berlin Novgorod Demichev Vologda Anadir Ravioli Ilotsk Kirov Voroneg Go'
+
+turn = 0
+words = text.lower().split()
+for i in range(1, len(words)):
+    turn += 1
+    if words[i][0] == words[i-1][-1]:
+        pass
+    else:
+        break
+if turn % 2 != 0:
+    print('Петя выиграл')
+else:
+    print('Вася выиграл')
+print(turn)
---
+++ Practicum8/3.py
@@ -0,0 +1,6 @@
+text = 'hahafffffffff text about spaces oo000000hohoh     '
+
+k = []
+for i in text:
+    k.append(i)
+print(len(set(k)))
